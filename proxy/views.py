aa06c87a5b9e472bfce4997d7b3a95989b40cc75
Never11n <never1n@ukr.net>
2024-01-23 22:46:15 +0200

    adding signup view

--- proxy/views.py
+++ proxy/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.contrib.auth import login
+from django.shortcuts import render, redirect
+from .forms import SignUpForm
 
-# Create your views here.
+
+def signup(request):
+    form = SignUpForm(request.POST)
+    if request.method == "POST":
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('main')
+    else:
+        form = SignUpForm()
+    return render(request, 'signup.html', {'form': form})
+
