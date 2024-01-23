ad46995f36773e41cd63c55b36cadd568a51c195
Never11n <never1n@ukr.net>
2024-01-23 23:55:55 +0200

    adding login system

--- proxy/views.py
+++ proxy/views.py
@@ -1,4 +1,4 @@
-from django.contrib.auth import login
+from django.contrib.auth import login, authenticate
 from django.shortcuts import render, redirect
 from .forms import SignUpForm
 
@@ -14,3 +14,16 @@ def signup(request):
         form = SignUpForm()
     return render(request, 'signup.html', {'form': form})
 
+
+def custom_login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('main')
+        else:
+            return render(request, 'login.html', {'error_message': 'Incorrect values'})
+    else:
+        return render(request, 'login.html')
